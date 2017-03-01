e773b83457095a5a07d677168cc131054a78e43c
Eric Berry <cavneb@gmail.com>
2017-03-01 11:03:16 -0700

    fix migration bug

--- db/migrate/20170301002920_create_pixel_fire_tags_triggers.rb
+++ db/migrate/20170301002920_create_pixel_fire_tags_triggers.rb
@@ -1,8 +1,8 @@
 class CreatePixelFireTagsTriggers < ActiveRecord::Migration[5.0]
   def change
     create_table :pixel_fire_tags_triggers do |t|
-      t.references :tag, foreign_key: true
-      t.references :trigger, foreign_key: true
+      t.integer :tag_id, null: false
+      t.integer :trigger_id, null: false
 
       t.timestamps
     end
--- lib/pixel_fire/version.rb
+++ lib/pixel_fire/version.rb
@@ -1,3 +1,3 @@
 module PixelFire
-  VERSION = '0.1.0'
+  VERSION = '0.1.1'
 end
